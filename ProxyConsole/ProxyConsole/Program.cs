01df6b6d779f071cce9d753a2ab60d32505ce6e1
Ian St. John <iangstjohn@gmail.com>
2018-11-20 00:31:37 -0500

    Proxy Console Update
    Got the console application to show the lights and all of the attributes from the Hue Bridge.

--- ProxyConsole/ProxyConsole/Program.cs
+++ ProxyConsole/ProxyConsole/Program.cs
@@ -23,13 +23,53 @@ namespace ProxyConsole {
             bri = ct = sat = hue = 0;
             alert = colormode = mode = "";
         }
+
+        public override string ToString() {
+            string output = String.Empty;
+            output += "State:";
+            output += "\n\tOn: " + on;
+            output += "\n\tBrightness: " + bri;
+            output += "\n\tCT: " + ct;
+            output += "\n\tSaturation: " + sat;
+            output += "\n\tHue: " + hue;
+            output += "\n\tAlert: " + alert;
+            output += "\n\tColor Mode: " + colormode;
+            output += "\n\tMode: " + mode;
+            output += "\n\tReachable: " + reachable;
+            return output;
+        }
     }
 
     public class HueLight {
         public HueState state { get; set; }
+        public string type { get; set; }
+        public string name { get; set; }
+        public string modelid { get; set; }
+        public string manufacturername { get; set; }
+        public string productname { get; set; }
+        public string uniqueid { get; set; }
+        public string swversion { get; set; }
+        public string configid { get; set; }
+        public string productid { get; set; }
 
         public HueLight() {
             state = new HueState();
+            type = name = modelid = manufacturername = productname = uniqueid = swversion = configid = productid = "";
+        }
+
+        public override string ToString() {
+            string output = String.Empty;
+            output += state;
+            output += "\nType: " + type;
+            output += "\nName: " + name;
+            output += "\nModel ID: " + modelid;
+            output += "\nManufacturer Name: " + manufacturername;
+            output += "\nProduct Name: " + productname;
+            output += "\nUnique ID: " + uniqueid;
+            output += "\nSoftware Ver: " + swversion;
+            output += "\nConfig ID: " + configid;
+            output += "\nProduct ID: " + productid;
+            return output;
         }
     }
 
@@ -52,7 +92,6 @@ namespace ProxyConsole {
     class Program {
         static void Main(string[] args) {
             HueBridge bridge = new HueBridge();
-            
 
             string html = String.Empty;
             using(StreamReader sr = new StreamReader(WebRequest.Create("https://discovery.meethue.com/").GetResponse().GetResponseStream())) {
@@ -62,28 +101,47 @@ namespace ProxyConsole {
             Console.WriteLine("{0}", bridge);
             string URL = "http://" + bridge.internalipaddress + "/api/ge4zFs2pzOSF2T7XdvBOVk0ujRPqSc2KlC69VGJn/";
 
-            bridge.lights.Add(new HueLight());
-            bridge.lights[0].state.on = false;
-            bridge.lights[0].state.ct = 5;
-            bridge.lights[0].state.sat = 100;
-            bridge.lights[0].state.hue = 15151;
-            bridge.lights[0].state.bri = 5;
-
-            StringContent content = new StringContent(JsonConvert.SerializeObject(bridge.lights[0].state));
+            //StringContent content = new StringContent(JsonConvert.SerializeObject(bridge.lights[0].state));
 
             HttpClient client = new HttpClient {
                 BaseAddress = new Uri(URL)
             };
 
             // Add an Accept header for JSON format.
-            client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
-        
-            
-            
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("lights/").Result;
+            dynamic dynJson = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result);
+            int lightCnt = 0;
+            foreach(var light in dynJson) {
+                foreach(var thing in light) {
+                    bridge.lights.Add(new HueLight());
+                    bridge.lights[lightCnt] = JsonConvert.DeserializeObject<HueLight>(Convert.ToString(thing));
+                    lightCnt++; 
+                }
+            }
+
+            int lightNum = -1;
+            do {
+                Console.Write("Enter Light #: ");
+                lightNum = Convert.ToInt32(Console.ReadLine());
+                if(lightNum >= 0 && lightNum < bridge.lights.Count) Console.WriteLine("{0}\n", bridge.lights[lightNum]);
+            } while(lightNum != -1);
+
+            /*
+            bridge.lights.Add(new HueLight());
+            bridge.lights[0].state.on = true;
+            bridge.lights[0].state.ct = 50;
+            bridge.lights[0].state.sat = 0;
+            bridge.lights[0].state.hue = 00000;
+            bridge.lights[0].state.bri = 254;   
+            */
+
             // List data response.
             //HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
-            HttpResponseMessage response = client.PutAsync("lights/5/state/", content).Result;
+            
+            //response = client.PutAsync("lights/5/state/", content).Result;
+            /*
             if(response.IsSuccessStatusCode) {
                 // Parse the response body.
                 string dataObjects = response.Content.ReadAsStringAsync().Result;  //Make sure to add a reference to System.Net.Http.Formatting.dll
@@ -91,17 +149,18 @@ namespace ProxyConsole {
             } else {
                 Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
             }
+            */
 
             //Make any other calls using HttpClient here.
-
+            /*
             while(true) {
-                Console.Read();
-                bridge.lights[0].state.on = !bridge.lights[0].state.on;
+                Console.Write("What # light to change: ");
+                int lightNum = Convert.ToInt32(Console.ReadLine());
+                bridge.lights[lightNum].state.on = !bridge.lights[lightNum].state.on;
                 content = new StringContent(JsonConvert.SerializeObject(bridge.lights[0].state));
-                response = client.PutAsync("lights/5/state/", content).Result;
+                response = client.PutAsync("lights/" + lightNum + "/state/", content).Result;
             }
-            
-
+            */
 
             //Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
             client.Dispose();
