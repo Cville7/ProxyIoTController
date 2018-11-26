0903c056dab24ee44b120cdc23a49c324d435ff9
Cville7 <danieldang1998@yahoo.com>
2018-11-26 04:14:19 -0500

    New App
    New Shell for App, removed old one due to certficate issues.

---
+++ ProxyMobile/ProxyMobile/ProxyMobile/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProxyMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            iot.Children.Clear();
+
+            List<int> testList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            foreach (int item in testList)
+            {
+
+                var btn = new Button()
+                {
+                    Text = item.ToString(),
+                    StyleId = item.ToString()
+                };
+
+                iot.Children.Add(btn);
+            }
+        }
+    }
+}
